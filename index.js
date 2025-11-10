c96c5c137f2d38a239d322d06e467030480e2c17
Arafat Hossain <arafathossain173737@gmail.com>
2025-11-10 12:10:53 +0600

    mongdb connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,9 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB URI & Client
 const uri =
+  process.env.MONGODB_URI ||
   "mongodb+srv://assignment10dbUser:lcFmjS5ESODyHTnx@cluster0.fo0mn81.mongodb.net/?appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,39 +22,25 @@ const client = new MongoClient(uri, {
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("assignment10 server is running");
-});
-
 async function run() {
   try {
     await client.connect();
     const db = client.db("smart_db");
-    const productsCollection = db.collection("products");
 
-    app.post("/products", async (req, res) => {
-      const newProduct = req.body;
-      const result = await productsCollection.insertOne(newProduct);
-      res.send(result);
-    });
+    // Collections
+    const listingsCollection = db.collection("listings");
+    const ordersCollection = db.collection("orders");
 
-    app.delete("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-  }
-}
+    
 
 run().catch(console.dir);
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Assignment10 server is running");
+});
+
+// Start server
 app.listen(port, () => {
-  console.log(`assignment10 server is running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
